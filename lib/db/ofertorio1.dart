2829bbd241f9c255ab0347d3f3748ac2f899611d
Danniel Augusto <ddiasguto@protonmail.com>
2023-05-13 21:10:52 -0300

    classes for chants created

--- lib/db/ofertorio1.dart
+++ lib/db/ofertorio1.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
-
-import '../main.dart';
+import '../templates/cipher.dart';
 
 class Musica extends StatefulWidget {
+  const Musica({super.key});
   @override
-  State<StatefulWidget> createState() {
-    int index;
-  }
+  State<StatefulWidget> createState() => _MusicaState();
+}
 
+class _MusicaState extends State<Musica> {
   @override
   Widget build(BuildContext context) {
     void _onItemTapped(int index) {
       index;
     }
+
+    int selectedIndex = 0;
 
     return MaterialApp(
       title: 'A ti Meu Deus',
@@ -30,128 +32,31 @@ class Musica extends StatefulWidget {
               label: 'Próxima',
             ),
           ],
-          currentIndex: _selectedIndex,
+          currentIndex: selectedIndex,
           selectedItemColor: Colors.redAccent,
           onTap: _onItemTapped,
         ),
-        body: Center(
+        body: SingleChildScrollView(
           child: SelectionArea(
             child: Column(
               children: const <Widget>[
                 Text(' '),
-                Text(
-                  'A TI, MEU DEUS',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22,
-                  ),
-                ),
-                Text(' '),
-                Text(
-                  'A Ti, meu Deus',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  'Elevo meu coração',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  'Elevo as minhas mãos',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  'Meu olhar, minha voz',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  '',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  'A Ti, meu Deus',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  'Eu quero oferecer',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  'Meus passos e meu viver',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  'Meus caminhos, meu sofrer',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  '',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  'A Tua ternura, Senhor, vem me abraçar',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  'E a Tua bondade infinita me perdoar',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  'Vou ser o Teu seguidor',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  'E Te dar o meu coração',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                Text(
-                  'Eu quero sentir o calor, de Tuas mãos',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
+                MusicTitle(title: 'A TI MEU DEUS'),
+                Cifra(cipher: '        F#m'),
+                Music(cipher: 'A Ti, meu Deus elevo meu coração'),
+                Cifra(cipher: '        E'),
+                Music(cipher: 'Elevo as minhas mãos'),
+                Cifra(cipher: '        E'),
+                Music(cipher: 'Meu olhar, minha voz'),
+                Music(cipher: 'A Ti, meu Deus'),
+                Music(cipher: 'Eu quero oferecer'),
+                Music(cipher: 'Meus passos e meu viver'),
+                Music(cipher: 'Meus caminhos, meu sofrer'),
+                Music(cipher: 'A Tua ternura, Senhor, vem me abraçar'),
+                Music(cipher: 'E a Tua bondade infinita me perdoar'),
+                Music(cipher: 'Vou ser o Teu seguidor'),
+                Music(cipher: 'E Te dar o meu coração'),
+                Music(cipher: 'Eu quero sentir o calor, de Tuas mãos'),
               ],
             ),
           ),
---
+++ lib/templates/cipher.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class MusicTitle extends StatelessWidget {
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: const TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 22,
+      ),
+    );
+  }
+
+  const MusicTitle({required this.title});
+}
+
+class Music extends StatelessWidget {
+  final String cipher;
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          cipher.toUpperCase(),
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+        Text(''),
+      ],
+    );
+  }
+
+  const Music({required this.cipher});
+}
+
+class Cifra extends StatelessWidget {
+  final String cipher;
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      children: [
+        Text(''),
+        Text(
+          cipher,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 12,
+            color: Color.fromARGB(255, 240, 9, 9),
+          ),
+        ),
+      ],
+    ));
+  }
+
+  const Cifra({required this.cipher});
+}
