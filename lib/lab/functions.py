ed3cdb6b5b5b0186123d5a12eff38f6ebeb73ecd
Danniel Augusto <ddiasguto@protonmail.com>
2023-05-20 19:23:40 -0300

    generator fully working

---
+++ lib/lab/functions.py
@@ -0,0 +1,41 @@
+import os
+
+def appendToFile(out,fileIn):
+    lines = fileIn.readlines()
+    for line in lines:
+        s = "const Music(cipher: \'" + line + "\'),"
+        out.write(s)
+        out.write('\n')
+
+def setingFileOut(index):
+    if index == 1:
+        out='../db/entrada.dart'
+    elif index == 2:
+        out='../db/ofertorio.dart'
+    elif index ==3:
+        out='../db/comunhao.dart'
+    return out
+
+def writeHeader(out,titulo):
+    s = 'class ' + titulo + ' extends StatelessWidget {'
+    out.write(s)
+    out.write('\n\t')
+    s='const ' + titulo + '({super.key});'
+    out.write(s)
+    out.write('\n\t@override')
+    out.write('\tWidget build(BuildContext context) {\n')
+    out.write('\t\treturn Column(children: [\n')
+
+def check():
+    if not os.path.isfile('aux.txt'):
+        print('Arquivo de entrada não encontrado.\nSaindo...')
+        exit()
+
+
+def deleteAux():
+    if os.path.isfile('aux.txt'):
+        os.remove('aux.txt')
+        print("Arquivo aux.txt deletado.")
+
+def writeFooter(out):
+      out.write(']);\n}\n}')
---
+++ lib/lab/generator.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+import functions
+import os
+import os.path
+    
+
+functions.check()
+
+file1 = open('aux.txt','r')
+
+titulo = input('Titulo da Musica:')
+
+category = int(input(print('Categorias:\n\t1-Entrada;\n\t2-Ofertorio;\n\t3-Comunhão;\nDigite a categoria:')))
+
+
+outPath = open(functions.setingFileOut(category),'a')
+
+functions.writeHeader(outPath, titulo)
+
+functions.appendToFile(outPath, file1)
+
+functions.writeFooter(outPath)
+
+outPath.close()
+
+file1.close()
+
+functions.deleteAux()
+
+
+
+#absolute path: /home/danniel/repositorios/diasguto/lib/db/
