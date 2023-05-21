af6491e01041276ed13c443134a02893d4512e73
Danniel Augusto <ddiasguto@protonmail.com>
2023-05-20 22:13:50 -0300

    criacao das listas de usicas para cada categoria

---
+++ lib/db/lists.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/widgets.dart';
+import 'comunhao.dart';
+import 'ofertorio.dart';
+import 'final.dart';
+import 'entrada.dart';
+
+const List<Widget> entrance = <Widget>[
+  TeAmareiSenhor(),
+  EisMeAquiSenhor(),
+  SenhorQuemEntrara(),
+  OProfeta(),
+  DiaDeFesta()
+];
+
+const List<Widget> offers = <Widget>[
+  AtiMeuDeus(),
+  MinhaVidaTemSentido(),
+  EstarEmTuasMaos(),
+  SeBendito()
+];
+
+const List<Widget> comunion = <Widget>[
+  ComoEsLindo(),
+  PelosPradosECampinas(),
+  ConhecoUmCoracao(),
+  PaoDaVida(),
+  CantarABelezaDaVida(),
+  SacramentoDaComunhao(),
+  VouCantarTeuAmor()
+];
+
+const List<Widget> ending = <Widget>[
+  VouNavegar(),
+  MariaDeMinhaInfancia(),
+  PelasEstradasDaVida(),
+  AEscolhida(),
+  ChuvaDeGraca()
+];
