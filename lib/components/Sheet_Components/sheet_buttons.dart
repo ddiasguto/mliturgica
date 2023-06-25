4c9ffcdc623d3d88fbce7c2dcab161272f7225d0
Danniel Augusto <ddiasguto@protonmail.com>
2023-06-25 14:59:38 -0300

    Sheet button modularized

---
+++ lib/components/Sheet_Components/sheet_buttons.dart
@@ -0,0 +1,124 @@
+import 'package:diasguto/pages/show_music.dart';
+import 'package:diasguto/provider/Maestro.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../widgets/bottom_sheet.dart';
+import '../../widgets/random_sheet_dialog.dart';
+
+Widget generateButton(context) {
+  return Container(
+    width: 300,
+    height: 45,
+    child: Card(
+      child: InkWell(
+        onTap: () {
+          showDialog<String>(
+            context: context,
+            builder: randomSheetDialog,
+          );
+        },
+        splashColor: Color.fromARGB(255, 116, 12, 12),
+        focusColor: Colors.black,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Gerar nova',
+              style: TextStyle(
+                color: Colors.green[800],
+                fontSize: 20,
+              ),
+            ),
+            Icon(
+              Icons.refresh,
+              color: Colors.green[800],
+              size: 30,
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+Widget acessShared(context) {
+  return Container(
+    width: 300,
+    height: 45,
+    child: Card(
+      child: InkWell(
+        onTap: () {
+          /*Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const ShowMusic()));*/
+        },
+        splashColor: Color.fromARGB(255, 116, 12, 12),
+        focusColor: Colors.black,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Acessar ',
+              style: TextStyle(
+                color: Colors.blue[800],
+                fontSize: 20,
+              ),
+            ),
+            Icon(
+              Icons.cloud_sync,
+              color: Colors.blue[800],
+              size: 30,
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+Widget navigateThroughSheet(context) {
+  Maestro maestro = Provider.of<Maestro>(context);
+
+  return Container(
+    width: 300,
+    height: 45,
+    child: Card(
+      child: InkWell(
+        onTap: () {
+          maestro.setLocalListToSheet();
+          maestro.setIndexToZero();
+          maestro.setCatalogueFalse();
+          maestro.setSheetTrue();
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const ShowMusic()));
+        },
+        onLongPress: () {
+          showModalBottomSheet<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return MyBottomSheet();
+            },
+          );
+        },
+        splashColor: Color.fromARGB(255, 116, 12, 12),
+        focusColor: Colors.black,
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Navegar pela partitura ',
+              style: TextStyle(
+                color: Color.fromARGB(255, 116, 12, 12),
+                fontSize: 20,
+              ),
+            ),
+            Icon(
+              Icons.arrow_circle_right,
+              color: Color.fromARGB(255, 116, 12, 12),
+              size: 30,
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
--- lib/pages/sheet_page.dart
+++ lib/pages/sheet_page.dart
@@ -1,10 +1,5 @@
-import 'package:diasguto/pages/show_music.dart';
-import 'package:diasguto/provider/Maestro.dart';
-import 'package:diasguto/widgets/bottom_sheet.dart';
+import 'package:diasguto/components/Sheet_Components/sheet_buttons.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import '../widgets/random_sheet_dialog.dart';
 
 class SheetPage extends StatefulWidget {
   const SheetPage({super.key});
@@ -16,84 +11,10 @@ class SheetPage extends StatefulWidget {
 class _SheetPageState extends State<SheetPage> {
   @override
   Widget build(BuildContext context) {
-    Maestro maestro = Provider.of<Maestro>(context);
     return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Container(
-        width: 300,
-        height: 45,
-        child: Card(
-          child: InkWell(
-            onTap: () {
-              maestro.setLocalListToSheet();
-              maestro.setIndexToZero();
-              maestro.setCatalogueFalse();
-              maestro.setSheetTrue();
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const ShowMusic()));
-            },
-            onLongPress: () {
-              showModalBottomSheet<void>(
-                context: context,
-                builder: (BuildContext context) {
-                  return MyBottomSheet();
-                },
-              );
-            },
-            splashColor: Color.fromARGB(255, 116, 12, 12),
-            focusColor: Colors.black,
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Navegar pela partitura ',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 116, 12, 12),
-                    fontSize: 20,
-                  ),
-                ),
-                Icon(
-                  Icons.arrow_circle_right,
-                  color: Color.fromARGB(255, 116, 12, 12),
-                  size: 30,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      Container(
-        width: 300,
-        height: 45,
-        child: Card(
-          child: InkWell(
-            onTap: () {
-              showDialog<String>(
-                context: context,
-                builder: randomSheetDialog,
-              );
-            },
-            splashColor: Color.fromARGB(255, 116, 12, 12),
-            focusColor: Colors.black,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Gerar nova',
-                  style: TextStyle(
-                    color: Colors.green[800],
-                    fontSize: 20,
-                  ),
-                ),
-                Icon(
-                  Icons.refresh,
-                  color: Colors.green[800],
-                  size: 30,
-                ),
-              ],
-            ),
-          ),
-        ),
-      )
+      navigateThroughSheet(context),
+      generateButton(context),
+      acessShared(context),
     ]);
   }
 }
