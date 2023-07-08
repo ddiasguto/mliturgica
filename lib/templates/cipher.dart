02d81b631bca03be8125e94ec4741c5db28f0f8f
Danniel Augusto <ddiasguto@protonmail.com>
2023-07-08 15:20:57 -0300

    break line at Music Widget

--- lib/templates/cipher.dart
+++ lib/templates/cipher.dart
@@ -23,21 +23,11 @@ class MusicTitle extends StatelessWidget {
 class Music extends StatelessWidget {
   final String cipher;
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                cipher.toUpperCase(),
-                style: const TextStyle(
-                  fontSize: 12,
-                ),
-              ),
-            ],
-          ),
-        ],
+    return Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        cipher.toUpperCase(),
+        softWrap: true,
       ),
     );
   }
@@ -45,27 +35,31 @@ class Music extends StatelessWidget {
   const Music({required this.cipher});
 }
 
+Widget Mus = const Padding(
+  padding: EdgeInsets.all(32),
+  child: Text(
+    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+    'Alps. Situated 1,578 meters above sea level, it is one of the '
+    'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+    'half-hour walk through pastures and pine forest, leads you to the '
+    'lake, which warms to 20 degrees Celsius in the summer. Activities '
+    'enjoyed here include rowing, and riding the summer toboggan run.',
+    softWrap: true,
+  ),
+);
+
 class Cifra extends StatelessWidget {
   final String cipher;
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              cipher,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 8,
-                color: Color.fromARGB(255, 240, 9, 9),
-              ),
-            ),
-          ],
-        ),
-      ],
-    ));
+    return Text(
+      textAlign: TextAlign.center,
+      cipher,
+      style: const TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 8,
+        color: Color.fromARGB(255, 240, 9, 9),
+      ),
+    );
   }
 
   const Cifra({required this.cipher});
