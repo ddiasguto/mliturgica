97d25d2951265eb92f5304947e5d5e92386b1546
Danniel Augusto <ddiasguto@protonmail.com>
2023-07-11 05:07:48 -0300

    class imgHome created

---
+++ lib/models/imgHome.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ImgHome extends StatelessWidget {
+  final String filename;
+
+  Widget build(context) {
+    return Image.asset(
+      'assets/home_img/$filename.jpg',
+      width: MediaQuery.of(context).size.width * .95,
+      height: MediaQuery.of(context).size.width * 0.473632812 * .95,
+      fit: BoxFit.cover,
+    );
+  }
+
+  const ImgHome({super.key, required this.filename});
+}
